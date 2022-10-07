2d82721bbdf828a0b0a3cafcf3f4fda1fc9c94dd
Maneh Seiranian <manehseiranian98@gmail.com>
2022-10-07 19:40:27 +0100

    Add app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__": # if app is the current one running then run my app
+    app.run()
+
+
+# my server is like a resturant at the corner of my laptop, but its closed now
