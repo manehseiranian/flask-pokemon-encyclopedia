d435c734d53d1f1006dc3ecf02466552329465ca
Maneh Seiranian <manehseiranian98@gmail.com>
2022-10-10 22:58:06 +0100

    Consolidate Pokemon dat aendpoints into a single function

--- app.py
+++ app.py
@@ -2,39 +2,49 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.get("/")# / is the root of our app this means if someone visits my app using a get reuqest and teh specific location they visit is the http://12223>>/, then we're gonna give them this recipe
+pokemon_creatures = {
+    "bilbasaur": "dinosaur",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole"
+}
+
+@app.get("/") # / is the root of our app this means if someone visits my app using a get reuqest and teh specific location they visit is the http://12223>>/, then we're gonna give them this recipe
 def pokemon_list():
     return "Charmander, pikachu, eevee, bilbasaur, diglett"
 
-@app.get("/bulbasaur")
-def bulbasaur_data():
-    return "this is bulbasaur generation 1 pokemon who looks like a little dinosaur"
+ #the lines below are v1 until I highlight that v1 is ending, commenting out to implemnet
 
-@app.get("/charmander")
-def charmander_data():
-    return "this is charmander generation 1 pokemon who looks like a little reptile"
-
-@app.get("/pikachu")
-def pikachu_data():
-    return "this is pikachu generation 1 pokemon who looks like a little rodent"
-
-@app.get("/eevee")
-def eevee_data():
-    return "this is eevee generation 1 pokemon who looks like a little fox"
-
-@app.get("/diglett")
-def diglett_data():
-    return "this is diglett generation 1 pokemon who looks like a little mole"
-
-
-
-#Menu item carbonara:
-# Beat the eggs
-# Chop the pancetta
-# Grate the cheese
-# Boil the pasta
-# Add everything to the pan
 #
+# @app.get("/bulbasaur")
+# def bulbasaur_data():
+#     return "this is bulbasaur generation 1 pokemon who looks like a little dinosaur"
+#
+# @app.get("/charmander")
+# def charmander_data():
+#     return "this is charmander generation 1 pokemon who looks like a little reptile"
+#
+# @app.get("/pikachu")
+# def pikachu_data():
+#     return "this is pikachu generation 1 pokemon who looks like a little rodent"
+#
+# @app.get("/eevee")
+# def eevee_data():
+#     return "this is eevee generation 1 pokemon who looks like a little fox"
+#
+# @app.get("/diglett")
+# def diglett_data():
+#     return "this is diglett generation 1 pokemon who looks like a little mole"
+#
+# end of v1
+
+
+
+@app.get("/<pokemon_name>") # <> means that in the browser whatever I type after / it's gonna recognise as a name
+def pokemon_data(pokemon_name):
+    creature = pokemon_creatures.get(pokemon_name)
+    return f"this is {pokemon_name}, a generation 1 pokemon who looks like a little {creature}"
 
 
 
