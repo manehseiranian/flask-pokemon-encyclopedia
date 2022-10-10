3621109d46927517121c556f216f84f1b4d96609
Maneh Seiranian <manehseiranian98@gmail.com>
2022-10-10 18:22:07 +0100

    Add more endpoints to app.py

--- app.py
+++ app.py
@@ -2,6 +2,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.get("/")# / is the root of our app this means if someone visits my app using a get reuqest and teh specific location they visit is the http://12223>>/, then we're gonna give them this recipe
+def pokemon_list():
+    return "Charmander, pikachu, eevee, bilbasaur, diglett"
+
+@app.get("/bulbasaur")
+def bulbasaur_data():
+    return "this is bulbasaur generation 1 pokemon who looks like a little dinosaur"
+
+@app.get("/charmander")
+def charmander_data():
+    return "this is charmander generation 1 pokemon who looks like a little reptile"
+
+@app.get("/pikachu")
+def pikachu_data():
+    return "this is pikachu generation 1 pokemon who looks like a little rodent"
+
+@app.get("/eevee")
+def eevee_data():
+    return "this is eevee generation 1 pokemon who looks like a little fox"
+
+@app.get("/diglett")
+def diglett_data():
+    return "this is diglett generation 1 pokemon who looks like a little mole"
+
+
+
+#Menu item carbonara:
+# Beat the eggs
+# Chop the pancetta
+# Grate the cheese
+# Boil the pasta
+# Add everything to the pan
+#
+
+
+
 if __name__ == "__main__": # if app is the current one running then run my app
     app.run()
 
