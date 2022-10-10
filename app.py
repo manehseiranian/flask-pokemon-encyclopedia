8f462aa159fe980c03d8e70c73cf4062851a328d
Maneh Seiranian <manehseiranian98@gmail.com>
2022-10-10 23:40:04 +0100

    Refactor Pokemon data endpoint to use external API

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-pokemon_creatures = {
-    "bilbasaur": "dinosaur",
-    "charmander": "reptile",
-    "pikachu": "rodent",
-    "eevee": "fox",
-    "diglett": "mole"
-}
+#the lines below are v2 until I highlight that v2 is ending, commenting out to implemnet
+# pokemon_creatures = {
+#     "bilbasaur": "dinosaur",
+#     "charmander": "reptile",
+#     "pikachu": "rodent",
+#     "eevee": "fox",
+#     "diglett": "mole"
+# }
+# </v2>
 
-@app.get("/") # / is the root of our app this means if someone visits my app using a get reuqest and teh specific location they visit is the http://12223>>/, then we're gonna give them this recipe
-def pokemon_list():
-    return "Charmander, pikachu, eevee, bilbasaur, diglett"
+
+# <v1> and <v2>
+# @app.get("/") # / is the root of our app this means if someone visits my app using a get reuqest and teh specific location they visit is the http://12223>>/, then we're gonna give them this recipe
+# def pokemon_list():
+#     return "Charmander, pikachu, eevee, bilbasaur, diglett"
+
+# </v1> and </v2>
 
  #the lines below are v1 until I highlight that v1 is ending, commenting out to implemnet
 
-#
 # @app.get("/bulbasaur")
 # def bulbasaur_data():
 #     return "this is bulbasaur generation 1 pokemon who looks like a little dinosaur"
@@ -40,12 +46,25 @@ def pokemon_list():
 # end of v1
 
 
+# <v2>
+# @app.get("/<pokemon_name>") # <> means that in the browser whatever I type after / it's gonna recognise as a name
+# def pokemon_data(pokemon_name):
+#     creature = pokemon_creatures.get(pokemon_name)
+#     return f"this is {pokemon_name}, a generation 1 pokemon who looks like a little {creature}"
+# </v2>
 
+#<v3>
 @app.get("/<pokemon_name>") # <> means that in the browser whatever I type after / it's gonna recognise as a name
 def pokemon_data(pokemon_name):
-    creature = pokemon_creatures.get(pokemon_name)
-    return f"this is {pokemon_name}, a generation 1 pokemon who looks like a little {creature}"
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+    pokemon = response.json() #casue we're assumin it's json data, which most often it will be
+    return f"This is {pokemon['name']}. \n" \
+           f"Height: {pokemon['height']}.\n" \
+           f"Weight: {pokemon['weight']}. \n" \
+           f"Base experience: {pokemon['base_experience']}.\n" \
+           f"Type(s): {'and'.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
+#</v2>
 
 
 if __name__ == "__main__": # if app is the current one running then run my app
